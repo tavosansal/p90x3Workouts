823825bef45217e15b165374c632a6efa15aa0ae
Gustavo San Jose <gustavo.sanjose@coschedule.com>
2020-04-13 15:55:29 -0500

    [CHORE] test

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -8,13 +8,14 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>About</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">About</IonTitle>
+            <p>test</p>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 3 page" />
