2158bff3d3adc4b75a490ba7ade54ee49a571e73
Gustavo San Jose <gustavo.sanjose@coschedule.com>
2020-04-25 13:15:46 -0500

    [CHORE] React sucks

--- src/components/CalendarEmpty.tsx
+++ src/components/CalendarEmpty.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
-import './ExploreContainer.css';
-import { IonButton, IonIcon } from '@ionic/react';
+import './CalendarEmpty.css';
+import { IonButton, IonIcon, IonDatetime } from '@ionic/react';
 import { calendarSharp } from 'ionicons/icons';
 
 const CalendarEmpty: React.FC = () => {
+  const [isSelectingDate, setIsSelectingDate] = useState(false);
+  const [selectedDate, setSelectedDate] = useState('');
+
+  function openPicker() {
+    const picker = document.querySelector('.picker');
+    // @ts-ignore
+    picker?.open();
+  }
+
+  function dateSelected(e: { detail: { value: any; }; }) {
+    setSelectedDate(e.detail.value!);
+  }
+
   return (
     <div className="container">
       <strong>You don't have a calendar yet.</strong>
@@ -11,10 +24,12 @@ const CalendarEmpty: React.FC = () => {
         Pick a starting day to generate your workout calendar.
       </p>
       <br />
-      <IonButton>
+      <IonButton onClick={openPicker}>
         <IonIcon slot="start" icon={calendarSharp} />
         Select Start Date
       </IonButton>
+
+      <IonDatetime className="picker" value={selectedDate} onIonChange={dateSelected} />
     </div>
   )
 }
